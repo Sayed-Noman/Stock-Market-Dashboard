c2cc6a8709a576d33a226beac7b7aae64e13e8ce
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-03-18 22:59:09 +0600

    basic skeleton of page added

--- yfinance_dashboard/dashboard.py
+++ yfinance_dashboard/dashboard.py
@@ -13,12 +13,40 @@ app =dash.Dash()
 app.layout= html.Div([
    #Navigation Bar 
    html.Div([
-       
+       #Dropdown Title
+        html.P("Chose a Ticker Name :", className ='start'),
+        #Dropdown List
+        dcc.Dropdown("Dropdown_Tickers", options = [
+                {"label" : "Apple", "value" :"AAPL" },
+                {"label" : "Tesla", "value" :"TSLA" }, 
+                {"label" : "Facebook", "value" :"FB" },              
+        ]),
+       #Dropdown Button
+       html.Div([
+            html.Button('Stock Price', className= 'stock-btn', id ='stock'),
+            html.Button('Indicators', className = 'indicator-btn', id = 'indicators'),
+        ], className='buttons'),
 
+    ], className = 'navigation'),
 
+    #Content Block
+    html.Div([
+        #Content Header
+        html.Div([
+            html.P(id='ticker'),
+            html.Img(id='logo'),
+        ], className='header'),
 
-    ], className = 'navigation') 
+        #Content Description
+        html.Div(id='description',className= 'description_ticker'),
 
-])
+        #Content Graphs
+        html.Div([
+            html.Div([], id ='graphs-content')
+        ], id = 'main-content')
+
+    ], className='content')
+
+], className = 'container')
 
 app.run_server(debug = True)
